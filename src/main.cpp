f06deb7a7570f90b6ae11c7c6b432af017db6d2e
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-05 17:33:55 +0200

    Add initial implementation of Game of Life with main and build scripts

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include <raylib.h>
+
+int main() {
+
+    Color GREY = {29, 29, 29, 255};
+    const int WINDOW_WIDTH = 750;
+    const int WINDOW_HEIGHT = 750;
+    int FPS = 12;
+
+    InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
+    SetTargetFPS(FPS);
+
+    // Simulation loop
+    while(WindowShouldClose() == false) {
+        // Event handling
+
+
+        // Updating state
+
+
+        // Drawing
+        BeginDrawing();
+        ClearBackground(GREY);
+        EndDrawing();
+
+    }
+
+    CloseWindow();
+    return 0;
+}
