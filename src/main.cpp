c764f03a7d7da1bb4f375f2a2d26758001e027c7
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-05 19:17:53 +0200

    Add controls to reset simulation and adjust FPS in main loop

--- src/main.cpp
+++ src/main.cpp
@@ -26,6 +26,22 @@ int main() {
                 SetWindowTitle("Game of Life - Running");
             }
         }
+        else if(IsKeyPressed(KEY_R)) {
+            simulation = Simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE); // Reset the simulation
+            SetWindowTitle("Game of Life - Reset");
+        }
+        else if(IsKeyPressed(KEY_W)) {
+            FPS += 2; // Increase FPS
+            SetTargetFPS(FPS);
+            SetWindowTitle(("Game of Life - Running at " + std::to_string(FPS) + " FPS").c_str());
+        }
+        else if(IsKeyPressed(KEY_S)) {
+            if(FPS > 2) {
+                FPS -= 2; // Decrease FPS
+                SetTargetFPS(FPS);
+                SetWindowTitle(("Game of Life - Running at " + std::to_string(FPS) + " FPS").c_str());
+            }
+        }
 
         // Updating state
         simulation.Update();
