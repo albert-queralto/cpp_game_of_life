dfd30dd01e01a26d2e98e4d910e9d7e03045863b
albert-queralto <albert.queralto.lopez@gmail.com>
2025-06-05 19:13:23 +0200

    Refactor main.cpp: adjust cell size and clean up example cell value settings

--- src/main.cpp
+++ src/main.cpp
@@ -7,26 +7,28 @@ int main() {
     Color GREY = {29, 29, 29, 255};
     const int WINDOW_WIDTH = 750;
     const int WINDOW_HEIGHT = 750;
-    const int CELL_SIZE = 10; // Size of each cell in pixels
+    const int CELL_SIZE = 4; // Size of each cell in pixels
     int FPS = 12;
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Game of Life");
     SetTargetFPS(FPS);
     Simulation simulation(WINDOW_WIDTH, WINDOW_HEIGHT, CELL_SIZE);
-    simulation.SetCellValue(5, 29, 1); // Example to set a cell value
-    simulation.SetCellValue(6, 0, 1); // Example to set a cell value
-    simulation.SetCellValue(5, 0, 1); // Example to set a cell value
-    simulation.SetCellValue(4, 0, 1); // Example to set a cell value
-
-    std::cout << simulation.CountLiveNeighbors(5, 5) << std::endl; // Example to count live neighbors
 
     // Simulation loop
     while(WindowShouldClose() == false) {
         // Event handling
-
+        if(IsKeyPressed(KEY_SPACE)) {
+            if(simulation.IsRunning()) {
+                simulation.Stop();
+                SetWindowTitle("Game of Life - Paused");
+            } else {
+                simulation.Start();
+                SetWindowTitle("Game of Life - Running");
+            }
+        }
 
         // Updating state
-
+        simulation.Update();
 
         // Drawing
         BeginDrawing();
